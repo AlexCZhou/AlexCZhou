c6bcb0746b53b139cab3886cda7d4681bedaa7aa
AlexCZhou <zbc1357@Hotmail.com>
2023-08-02 00:13:18 +0800

    Update init.lua
    增加一个启动界面插件dashboard-nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -191,7 +191,53 @@ local plugins = {
             require("lualine").setup();
         end,
     },
-    
+    -- dashboard-nvim
+    -- Fancy and Blazing Fast start screen plugin of Neovim
+    {
+        "glepnir/dashboard-nvim",
+        event = "VimEnter",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons"
+        },
+        config = function()
+            require("dashboard").setup({
+                theme = "hyper",
+                config = {
+                    header = { 
+                        "TypeAlpha's                                            ",
+                        "                                                       ",
+                        " ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
+                        " ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
+                        " ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
+                        " ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
+                        " ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
+                        " ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
+                        "                                                       ",
+                    },
+                    shortcut = {
+                        { 
+                            desc = "󰒲 Lazy", 
+                            group = "@property",
+                            action = require("lazy").home,
+                            key = "l"
+                        }
+                    },
+                    packages = {
+                        enable = false,
+                    },
+                    project = {
+                        enable = true,
+                        limit = 6,
+                        action = function(path)
+                            vim.cmd("e "..path); -- nvim-tree.lua will take over.
+                        end,
+                    },
+                    mru = { limit = 9 },
+                    footer = {},
+                },
+            });
+        end
+    }
 }
 require("lazy").setup(plugins, opts);
 
