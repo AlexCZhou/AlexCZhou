73833d1671c551d3e618009ce6a45b6e7a49a45e
AlexCZhou <zbc1357@Hotmail.com>
2023-11-28 10:31:09 +0800

    增加barbar.nvim插件

--- nvim/init.lua
+++ nvim/init.lua
@@ -71,7 +71,7 @@ local plugins = {
     -- AI code generation, buggy for now.
     {
         "github/copilot.vim",
-        lazy = true,
+        lazy = false,
         opts = {},
         config = function()
 
@@ -273,6 +273,39 @@ local plugins = {
         "tpope/vim-fugitive",
         config = function()
         end,
+    },
+    -- barbar.nvim
+    {
+        "romgrk/barbar.nvim",
+        dependencies = {
+            "lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+            "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+        },
+        init = function() 
+            vim.g.barbar_auto_setup = true
+        end,
+        config = function()
+            local map = vim.api.nvim_set_keymap
+            local opts = { noremap = true, silent = true }
+
+            -- Move to previous/next
+            map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+            map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+            -- Re-order to previous/next
+            map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+            map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+            -- Pin/unpin buffer
+            map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+            -- Close buffer
+            map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+        end,
+        opts = {
+            -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+            -- animation = true,
+            -- insert_at_start = true,
+            -- …etc.
+        },
+        version = '^1.0.0', -- optional: only update when a new 1.x version is released
     }
 }
 require("lazy").setup(plugins, opts);
