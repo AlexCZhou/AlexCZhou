557840aa08c2b41b7911b586035f9e8dc83a587a
AlexCZhou <zbc1357@Hotmail.com>
2024-02-17 14:44:46 +0800

    添加复制高亮、修复调用错误

--- nvim/lua/typealpha/init.lua
+++ nvim/lua/typealpha/init.lua
@@ -4,7 +4,7 @@ require("typealpha.lazy")
 
 -- 属于每台机器自己的配置
 -- 只在文件存在时才加载
-if vim.fn.filereadable(vim.fn.expand("~/.local.vim")) then
+if vim.fn.filereadable(vim.fn.expand("./local.lua")) == 1 then
     require("typealpha.local")
 end
 
--- nvim/lua/typealpha/set.lua
+++ nvim/lua/typealpha/set.lua
@@ -18,3 +18,13 @@ vim.opt.signcolumn = "yes" -- for lsp
 -- GUI
 vim.opt.guifont = "JetBrainsMonoNL Nerd Font Mono:h11"
 
+
+-- highlight on yank
+-- highlight yanked text for 200ms using the "Visual" highlight group
+vim.cmd[[
+    augroup highlight_yank
+    autocmd!
+    au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=200})
+    augroup END
+]]
+
